a4ae1dffe237e5649e2d38253725dc5040990a4d
bob sagat <wiiqwertyuiop@gmail.com>
2024-05-26 22:56:38 -0400

    fixed cookie

--- src/controllers/contact/contact.controller.go
+++ src/controllers/contact/contact.controller.go
@@ -9,11 +9,6 @@ import (
 	"net/url"
 )
 
-// Example
-var exampleContact = contact.Contact{
-	FirstName: "Joe", LastName: "Blow", Email: "joe@blow.com",
-}
-
 var Controller = controller.Controller{
 	Route:        "/contact",
 	NeedsReferer: true,
@@ -23,33 +18,41 @@ var Controller = controller.Controller{
 var paths = []controller.Path{
 
 	controller.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		// Cookie test
-		// Read saved data from cookie
-		if cookie, err := r.Cookie("contact"); err == nil {
-			decoded, _ := url.QueryUnescape(cookie.Value)
-			json.Unmarshal([]byte(decoded), &exampleContact)
-		}
-		renderer.RenderView(w, "contact/form.html", exampleContact)
+		renderer.RenderView(w, "contact/form.html", getContact(r))
 	}),
 
 	controller.Get("/edit", func(w http.ResponseWriter, r *http.Request) {
-		renderer.RenderView(w, "contact/editForm.html", exampleContact)
+		renderer.RenderView(w, "contact/editForm.html", getContact(r))
 	}),
 
 	controller.Put("/", func(w http.ResponseWriter, r *http.Request) {
 		// Read form data
 		r.ParseForm()
-		exampleContact = contact.Contact{
+		newContact := contact.Contact{
 			FirstName: r.FormValue("firstName"),
 			LastName:  r.FormValue("lastName"),
 			Email:     r.FormValue("email"),
 		}
 
 		// Save data to cookie
-		data, _ := json.Marshal(exampleContact)
+		data, _ := json.Marshal(newContact)
 		http.SetCookie(w, &http.Cookie{Name: "contact", Value: url.QueryEscape(string(data))})
 
 		// Render page
-		renderer.RenderView(w, "contact/form.html", exampleContact)
+		renderer.RenderView(w, "contact/form.html", newContact)
 	}),
+}
+
+// For demo purposes
+func getContact(r *http.Request) contact.Contact {
+	// Cookie test
+	if cookie, err := r.Cookie("contact"); err == nil {
+		saved := contact.Contact{}
+		decoded, _ := url.QueryUnescape(cookie.Value)
+		json.Unmarshal([]byte(decoded), &saved)
+		return saved
+	}
+	return contact.Contact{
+		FirstName: "Joe", LastName: "Blow", Email: "joe@blow.com",
+	}
 }
